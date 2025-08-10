de1c6cdc48ee26cf818e2bdbf3f333621aafae6e
Connor Garey <connorgarey1999@gmail.com>
2025-08-11 00:17:52 +0100

    No longer in src/ and have been moved to /src/core

--- src/error_handling.zig
+++
@@ -1,9 +0,0 @@
-const std = @import("std");
-const sdl = @cImport(@cInclude("SDL3/SDL.h"));
-
-pub const ErrorHandle = struct {
-    pub fn sdlError() []const u8 {
-        const error_message: []const u8 = std.mem.sliceTo(sdl.SDL_GetError(), 0);
-        return error_message;
-    } 
-};
