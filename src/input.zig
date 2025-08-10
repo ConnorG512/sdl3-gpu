a6d6a0f5e63bff4a30d3ada3a4d2760d9d46092e
Connor Garey <connorgarey1999@gmail.com>
2025-08-10 23:49:53 +0100

    Recieving input now appears in it's own struct

--- src/input.zig
+++ src/input.zig
@@ -1,5 +1,15 @@
 const sdl = @cImport(@cInclude("SDL3/SDL.h"));
 
 pub const Input = struct {
+    current_event: sdl.SDL_Event = undefined,
 
+    pub fn pollForEvent(self: *Input) void {
+        while (true) {
+            if (sdl.SDL_PollEvent(&self.current_event)) {
+                if (self.current_event.type == sdl.SDL_EVENT_QUIT) {
+                    break;
+                }
+            }
+        }
+    }
 };
