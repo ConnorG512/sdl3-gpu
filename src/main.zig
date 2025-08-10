15242f85446b0b04c7118f9bd305a05c3b1d55c5
Connor Garey <connorgarey1999@gmail.com>
2025-08-11 00:17:23 +0100

    Moved properties out of main and changed window size

--- src/main.zig
+++ src/main.zig
@@ -1,27 +1,21 @@
 const std = @import("std");
-const erh = @import("error_handling.zig").ErrorHandle;
-const Window = @import("window.zig").Window;
-const Input = @import("input.zig").Input;
+
+const erh = @import("core/error_handling.zig").ErrorHandle;
+const Window = @import("core/window.zig").Window;
+const Input = @import("core/input.zig").Input;
+const GameProperties = @import("core/properties.zig").Properties;
 
 const sdl = @cImport({
     @cInclude("SDL3/SDL.h");
     @cInclude("SDL3/SDL_main.h");
 });
 
-const GameError = error {
-    CouldNotCreateAppMetadata,
-};
-
-const GameProperties = struct {
-    app_name: [:0]const u8 = "Mobile bounce game.",
-    version: [:0]const u8 = "0.0.1",
-    app_identifier: [:0]const u8 = "com.app.mobilebouncegame",
-};
-
-
 pub fn main() !void {    
     const game_props = comptime GameProperties {};
-    var win_ren = Window {};
+    var win_ren = Window {
+        .window_height = 720,
+        .window_width = 1280,       
+    };
 
     const metadata_res = sdl.SDL_SetAppMetadata(game_props.app_name, game_props.version, game_props.app_identifier);
     if (!metadata_res) {
