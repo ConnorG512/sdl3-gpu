c1619eec90d9aecf828f670d8e889d15fa176eb1
Connor Garey <connorgarey1999@gmail.com>
2025-08-11 01:13:23 +0100

    Added startGPU and claimWindow

--- src/gpu/gpu_compute.zig
+++ src/gpu/gpu_compute.zig
@@ -4,16 +4,31 @@ const sdl = @cImport(@cInclude("SDL3/SDL.h"));
 
 const GPUError = error {
     FailedToCreateContext,
+    CannotClaimWindow,
 };
 
 pub const GPUCompute = struct {
     gpu_context: ?*sdl.SDL_GPUDevice = null,
 
-    pub fn createDevice(self: *GPUCompute) !void {
+    pub fn startGPU(self: *GPUCompute, window: ?*sdl.SDL_Window) !void {
+        std.debug.assert(window != null);
+
+        try self.createDevice();
+        try self.claimWindow(window);
+    }
+
+    fn createDevice(self: *GPUCompute) !void {
         self.gpu_context = sdl.SDL_CreateGPUDevice(sdl.SDL_GPU_SHADERFORMAT_SPIRV, true, "vulkan");
         if (self.gpu_context == null) {
             std.log.err("Failed to create GPU context! {s}.", .{Error.sdlError()});
             return error.FailedToCreateContext;
+        }
+    }
+
+    fn claimWindow(self: *GPUCompute, window: ?*sdl.SDL_Window) !void {
+        const result = sdl.SDL_ClaimWindowForGPUDevice(self.gpu_context, window);
+        if (!result) {
+            return error.CannotClaimWindow;
         }
     }
 };
