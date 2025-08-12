41c0586574f14bacf1e45c410e74f72ab801478f
Connor Garey <connorgarey1999@gmail.com>
2025-08-12 13:01:09 +0100

    Removed stdout from logger function

--- src/core/logger.zig
+++ src/core/logger.zig
@@ -17,18 +17,15 @@ pub const Logger = struct {
     pub fn writeLog(self: *Logger, log_type: LogType, message: []const u8) !void {
         switch (log_type) {
             .Info => {
-                std.log.debug("Info Logging.", .{});
                 _ = try self.log_file.write("INFO: ");
                 _ = try self.log_file.write(message);
             },
             .Warning => {
-                std.log.debug("Warning Logging.", .{});
                 _ = try self.log_file.write("WARNING: ");
                 _ = try self.log_file.write(message);
 
             },
             .Error => {
-                std.log.debug("Error Logging.", .{});
                 _ = try self.log_file.write("ERROR: ");
                 _ = try self.log_file.write(message);
 
